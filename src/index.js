118541b87030099bd4a6ad093b6a643a2cc75ed7
zmh <zhengmh@gzsendi.cn>
2019-11-04 10:37:57 +0800

    chore:新增配置文件、入口文件

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// 获取数据类型
-(function (root, globalName, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD:
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node:
-        module.exports = factory();
-        // Use module export as simulated ES6 default export:(将模块导出用作模拟ES6默认导出)
-        module.exports.default = module.exports;
-    } else {
-        // Browser:
-        window[globalName] = factory();
-    }
-}(this, 'dataType', function () {
-    'use strict';
+import Button from './components/button/index';
 
-    return function dataType(data) {
-        return ({}).toString.call(data).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-    }
-}));
+export { Button };
